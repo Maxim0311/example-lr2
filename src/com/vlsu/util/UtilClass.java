6b4fee88fcf536756352f0cee874b4a9d7a88bc4
Maxim0311 <maxim0311@mail.ru>
2021-03-09 16:19:35 +0300

    Initial commit

---
+++ src/com/vlsu/util/UtilClass.java
@@ -0,0 +1,10 @@
+package com.vlsu.util;
+
+public class UtilClass {
+    public UtilClass() {
+        System.out.println("The instance of UtilClass created successfully...");
+    }
+    public String getAnswer() {
+        return "42";
+    }
+}
